48d3dd9b4eb6a678c53fd5a5d1511483ccf07df8
Rafael Souza <rpsouza.dev@gmail.com>
2023-06-30 21:13:50 -0300

    Add theme types and google fonts roboto

--- ignite-teams/App.tsx
+++ ignite-teams/App.tsx
@@ -1,11 +1,24 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components/native'
+import { ActivityIndicator } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import theme from './src/theme'
+
 import { Groups } from '@screens/Groups'
-import theme from 'src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }
