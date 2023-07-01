015f29574eccacae97822b1741a8890a6d70c3d8
Rafael Souza <rpsouza.dev@gmail.com>
2023-07-01 20:09:45 -0300

    Add list of groups

--- ignite-teams/src/screens/Groups/index.tsx
+++ ignite-teams/src/screens/Groups/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 
@@ -5,11 +7,19 @@ import * as S from './styles'
 import { GroupCartd } from '@components/GroupCard'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Itech'])
+
   return (
     <S.Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCartd title="Galera da Itech" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCartd key={`title-${item}`} title={item} />
+        )}
+      />
     </S.Container>
   )
 }
