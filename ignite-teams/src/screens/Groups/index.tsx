dadfa5109a1afcea686cf24d62e353842c161bc6
Rafael Souza <rpsouza.dev@gmail.com>
2023-07-01 14:37:16 -0300

    GroupCard component created

--- ignite-teams/src/screens/Groups/index.tsx
+++ ignite-teams/src/screens/Groups/index.tsx
@@ -2,12 +2,14 @@ import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 
 import * as S from './styles'
+import { GroupCartd } from '@components/GroupCard'
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <GroupCartd title="Galera da Itech" />
     </S.Container>
   )
 }
