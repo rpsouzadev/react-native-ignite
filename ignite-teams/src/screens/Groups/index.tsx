f80a314f51d4afa34f20512fa48e9d8acf7874a8
Rafael Pereira <rpsouza.dev@gmail.com>
2023-07-24 16:33:47 -0300

    Add navigation structure in Groups screen

--- ignite-teams/src/screens/Groups/index.tsx
+++ ignite-teams/src/screens/Groups/index.tsx
@@ -6,9 +6,16 @@ import { Button } from '@components/Button'
 import { Highlight } from '@components/Highlight'
 import { ListEmpty } from '@components/ListEmpty'
 import { GroupCartd } from '@components/GroupCard'
+import { useNavigation } from '@react-navigation/native'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <S.Container>
@@ -25,7 +32,7 @@ export function Groups() {
           <ListEmpty message="Adicione uma turma para jogar!" />
         )}
       />
-      <Button title="Criar nova turma" onPress={} />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </S.Container>
   )
 }
