41cb945d34006781c201527734f80396359b0757
Rafael Pereira <rpsouza.dev@gmail.com>
2023-07-24 17:27:32 -0300

    Passin parameters through the route

--- ignite-teams/src/screens/NewGroup/index.tsx
+++ ignite-teams/src/screens/NewGroup/index.tsx
@@ -4,12 +4,14 @@ import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
 
 export function NewGroup() {
+  const [group, setGroup] = useState('')
   const navigation = useNavigation()
 
   function handleNew() {
-    navigation.navigate('players', { group: 'Itech' })
+    navigation.navigate('players', { group })
   }
 
   return (
@@ -21,7 +23,7 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="Crie uma turma para adicionar as pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </S.Content>
     </S.Container>
--- ignite-teams/src/screens/Players/index.tsx
+++ ignite-teams/src/screens/Players/index.tsx
@@ -9,17 +9,23 @@ import { Highlight } from '@components/Highlight'
 import { ListEmpty } from '@components/ListEmpty'
 import { PlayerCard } from '@components/PlayerCard'
 import { ButtonIcon } from '@components/ButtonIcon'
+import { useRoute } from '@react-navigation/native'
+
+type RouteParams = {
+  group: string
+}
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState([])
+
+  const route = useRoute()
+  const { group } = route.params as RouteParams
+
   return (
     <S.Container>
       <Header showBackButton />
-      <Highlight
-        title="Nome da turma"
-        subtitle="Adicione a galera e separe os times"
-      />
+      <Highlight title={group} subtitle="Adicione a galera e separe os times" />
       <S.Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
