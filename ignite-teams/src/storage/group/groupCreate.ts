61fea9f7322384845087c0a6e9cca5533e2516c6
Rafael Pereira <rpsouza.dev@gmail.com>
2023-07-25 11:15:09 -0300

    Created custom error class

--- ignite-teams/src/storage/group/groupCreate.ts
+++ ignite-teams/src/storage/group/groupCreate.ts
@@ -2,10 +2,15 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { GROUP_COLLECTION } from '../storageConfig'
 import { groupsGetAll } from './groupsGetAll'
+import { AppError } from '@utils/AppErro'
 
 export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll()
+    const groupAlreadyExists = storedGroups.includes(newGroup)
+    if (groupAlreadyExists) {
+      throw new AppError('Já existe uma turma com esse nome')
+    }
     const storage = JSON.stringify([...storedGroups, newGroup])
     await AsyncStorage.setItem(GROUP_COLLECTION, storage)
   } catch (error) {
