1169fc5efc7c9cd3b959c42a18301805fc6c0a72
Rafael Pereira <rpsouza.dev@gmail.com>
2023-07-26 17:50:16 -0300

    Fonts Roboto, StatusBar and Assets

--- ignite-gym/App.tsx
+++ ignite-gym/App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <Text style={{ fontFamily: 'Roboto_700Bold', color: '#fff' }}>
+          Hello World!
+        </Text>
+      ) : (
+        <Text>Carregando...</Text>
+      )}
+    </View>
+  )
+}
