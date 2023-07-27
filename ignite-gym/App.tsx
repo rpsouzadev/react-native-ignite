36755a7d78762317752668b74fab85d3c58accd6
Rafael Pereira <rpsouza.dev@gmail.com>
2023-07-27 11:54:31 -0300

    Created SingnIn screen and add background img and logo svg

--- ignite-gym/App.tsx
+++ ignite-gym/App.tsx
@@ -1,12 +1,14 @@
 import { NativeBaseProvider } from 'native-base'
-import { StatusBar, View } from 'react-native'
-import { Loading } from '@components/Loading'
-import { THEME } from './src/theme'
+import { StatusBar } from 'react-native'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
+import { THEME } from './src/theme'
+import { SingIn } from '@screens/SingIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -18,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {!fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <SingIn /> : <Loading />}
     </NativeBaseProvider>
   )
 }
