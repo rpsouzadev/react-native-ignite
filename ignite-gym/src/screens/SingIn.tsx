2d44a26ce8acd3266920d2d98e9be59ee553d67d
Rafael Pereira <rpsouza.dev@gmail.com>
2023-07-27 14:20:52 -0300

    Finished SingIn screen and created the Input and button components

--- ignite-gym/src/screens/SingIn.tsx
+++ ignite-gym/src/screens/SingIn.tsx
@@ -1,25 +1,56 @@
-import { VStack, Image, Text, Center } from 'native-base'
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
+import { Button } from '@components/Button'
 import LogoSvg from '@assets/logo.svg'
+import { Input } from '@components/Input'
 
 export function SingIn() {
   return (
-    <VStack flex={1} bg="gray.700">
-      <Image
-        source={BackgroundImg}
-        alt="Pessoas treinando"
-        resizeMode="contain"
-        position="absolute"
-      />
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
 
-      <Center my={24}>
-        <LogoSvg />
+        <Center my={24}>
+          <LogoSvg />
 
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
-    </VStack>
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Acesse sua conta
+          </Heading>
+
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" secureTextEntry />
+
+          <Button title="Acessar" />
+        </Center>
+
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+
+          <Button title="Criar conta" variant="outline" />
+        </Center>
+      </VStack>
+    </ScrollView>
   )
 }
