115ccd159b1c8c09ea7453690dd53aad580a7172
Rafael Souza <rpsouza.dev@gmail.com>
2023-07-29 13:40:36 -0300

    Add icons to navigation bottom

--- ignite-gym/src/routes/app.routes.tsx
+++ ignite-gym/src/routes/app.routes.tsx
@@ -1,11 +1,17 @@
+import { useTheme } from 'native-base'
 import {
-  createNativeStackNavigator,
-  NativeStackNavigationProp,
-} from '@react-navigation/native-stack'
-import { Exercise } from '@screens/Exercise'
-import { History } from '@screens/History'
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from '@react-navigation/bottom-tabs'
+
+import HomeSvg from '@assets/home.svg'
+import HistorySvg from '@assets/history.svg'
+import ProfileSvg from '@assets/profile.svg'
+
 import { Home } from '@screens/Home'
+import { History } from '@screens/History'
 import { Profile } from '@screens/Profile'
+import { Exercise } from '@screens/Exercise'
 
 type AppRoutes = {
   home: undefined
@@ -14,17 +20,45 @@ type AppRoutes = {
   profile: undefined
 }
 
-export type AppNavigatorRoutesProps = NativeStackNavigationProp<AppRoutes>
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 
-const { Navigator, Screen } = createNativeStackNavigator<AppRoutes>()
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
+  const { sizes } = useTheme()
+
+  const iconSize = sizes[6]
+
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="home" component={Home} />
+    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
       <Screen name="exercise" component={Exercise} />
-      <Screen name="history" component={History} />
-      <Screen name="profile" component={Profile} />
     </Navigator>
   )
 }
--- ignite-gym/src/routes/index.tsx
+++ ignite-gym/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
 import { Box, useTheme } from 'native-base'
 
 export function Routes() {
@@ -11,7 +12,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
