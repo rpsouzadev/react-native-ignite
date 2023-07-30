fd5fa801a134b91dace6a7997f1b101f97ca1734
Rafael Souza <rpsouza.dev@gmail.com>
2023-07-29 21:31:57 -0300

    Created HomeHeader

---
+++ ignite-gym/src/components/HomeHeader.tsx
@@ -0,0 +1,32 @@
+import { HStack, Text, Heading, VStack, Icon } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native'
+
+import UserPhoto from './UserPhoto'
+
+export default function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: 'https://github.com/muglly.png' }}
+        alt="foto do usuario"
+        size={16}
+        mr={4}
+      />
+
+      <VStack flex={1}>
+        <Text color={'gray.100'} fontSize="md">
+          Olá
+        </Text>
+
+        <Heading color={'gray.100'} fontSize="md">
+          Rafel Souza
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  )
+}
