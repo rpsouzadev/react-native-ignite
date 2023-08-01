d0ef0e2c2a01686c41cddebc7bd9ca79005f4d6f
Rafael Pereira <rpsouza.dev@gmail.com>
2023-08-01 17:37:32 -0300

    Add react hook form initial structure in SignUp inputs

--- ignite-gym/src/screens/SignUp.tsx
+++ ignite-gym/src/screens/SignUp.tsx
@@ -1,18 +1,24 @@
 import { useNavigation } from '@react-navigation/native'
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import { useForm, Controller } from 'react-hook-form'
 
-import BackgroundImg from '@assets/background.png'
-import { Button } from '@components/Button'
 import LogoSvg from '@assets/logo.svg'
+import BackgroundImg from '@assets/background.png'
+
 import { Input } from '@components/Input'
+import { Button } from '@components/Button'
 
 export function SignUp() {
+  const { control } = useForm()
+
   const navigation = useNavigation<AuthNavigatorRoutesProps>()
 
   function hadleGoBack() {
     navigation.goBack()
   }
+
+  function handleSignUp() {}
 
   return (
     <ScrollView
@@ -41,20 +47,58 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input placeholder="Nome" />
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input placeholder="Nome" onChangeText={onChange} value={value} />
+            )}
           />
 
-          <Input placeholder="Senha" secureTextEntry />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
 
-          <Button title="Criar e acessar" />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password_confirm"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Confirme a senha"
+                secureTextEntry
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Button title="Criar e acessar" onPress={handleSignUp} />
         </Center>
 
-        <Center mt={24} flex={1} justifyContent="flex-end">
+        <Center mt={16} flex={1} justifyContent="flex-end">
           <Button
             title="Voltar para o login"
             variant="outline"
