9911d13949c6521ae3513b79c3cf576ecd45cef8
Rafael Souza <rpsouza.dev@gmail.com>
2023-08-06 19:37:59 -0300

    Loading logged in user

--- ignite-gym/mobile/src/contexts/AuthContext.tsx
+++ ignite-gym/mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, createContext, useState } from 'react'
+import { storageUserSave, storageUserGet } from '@storage/storageUser'
+import { ReactNode, createContext, useEffect, useState } from 'react'
 import { UserDTO } from '@dtos/UserDTO'
 import { api } from '@services/api'
 
@@ -25,11 +26,24 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user)
+        storageUserSave(data.user)
       }
     } catch (error) {
       throw error
     }
   }
+
+  async function loadUserData() {
+    const userLogged = await storageUserGet()
+
+    if (userLogged) {
+      setUser(userLogged)
+    }
+  }
+
+  useEffect(() => {
+    loadUserData()
+  }, [])
 
   return (
     <AuthContext.Provider value={{ user, signIn }}>
