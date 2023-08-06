ce044d28293f3c5579251e17c9d4e6bbd26f2ce0
Rafael Souza <rpsouza.dev@gmail.com>
2023-08-06 08:06:39 -0300

    How to create a context

--- ignite-gym/mobile/App.tsx
+++ ignite-gym/mobile/App.tsx
@@ -9,6 +9,7 @@ import {
 import { Routes } from '@routes/index'
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading'
+import { AuthContext } from '@contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -20,7 +21,16 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <AuthContext.Provider
+        value={{
+          id: '1',
+          name: 'Rodrigo',
+          email: 'rodrigo@email.com',
+          avatar: 'rodrigo.png',
+        }}
+      >
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContext.Provider>
     </NativeBaseProvider>
   )
 }
--- ignite-gym/mobile/src/routes/index.tsx
+++ ignite-gym/mobile/src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { Box, useTheme } from 'native-base'
 import { AuthRoutes } from './auth.routes'
 import { AppRoutes } from './app.routes'
-import { Box, useTheme } from 'native-base'
+
+import { AuthContext } from '@contexts/AuthContext'
+import { useContext } from 'react'
 
 export function Routes() {
   const { colors } = useTheme()
+
+  const contextData = useContext(AuthContext)
+  console.log('Usuario logado => ', contextData)
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
