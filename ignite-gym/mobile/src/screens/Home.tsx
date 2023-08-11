d623aab356df1bb11bd63a64762e13666f09cd72
Rafael Pereira <rpsouza.dev@gmail.com>
2023-08-11 16:40:31 -0300

    Searching and showing list of groups

--- ignite-gym/mobile/src/screens/Home.tsx
+++ ignite-gym/mobile/src/screens/Home.tsx
@@ -1,27 +1,50 @@
-import { HStack, VStack, FlatList, Heading, Text } from 'native-base'
+import { HStack, VStack, FlatList, Heading, Text, useToast } from 'native-base'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 import { useNavigation } from '@react-navigation/native'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
 
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
-  const [groups, setGroups] = useState([
-    'Costas',
-    'Bíceps',
-    'Trícipes',
-    'Ombro',
-  ])
+  const [groups, setGroups] = useState<string[]>([])
   const [exercises, setExercises] = useState(['1', '2', '3', '4', '5'])
   const [groupSelected, setGroupSelected] = useState('costas')
 
+  const toast = useToast()
   const navigation = useNavigation<AppNavigatorRoutesProps>()
 
   function handleOpenExerciseDetails() {
     navigation.navigate('exercise')
   }
+
+  async function fetchGroups() {
+    try {
+      const response = await api.get('/groups')
+
+      setGroups(response.data)
+      console.log('response.data =>', response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi posivel carregar os grupos musculares.'
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetchGroups()
+  }, [])
 
   return (
     <VStack flex={1}>
@@ -41,7 +64,7 @@ export function Home() {
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
-        _contentContainerStyle={{ px: 4 }}
+        _contentContainerStyle={{ px: 8 }}
         my={10}
         maxH={10}
         minH={10}
