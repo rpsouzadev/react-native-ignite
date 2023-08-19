697f626bbccc995b45edefdafb985a91e5c477e6
Rafael Souza <rpsouza.dev@gmail.com>
2023-08-19 12:28:43 -0300

    Add resent the request

--- ignite-gym/mobile/src/services/api.ts
+++ ignite-gym/mobile/src/services/api.ts
@@ -59,6 +59,7 @@ api.registerInterceptTokenManager = (signOut) => {
 
           isRefreshing = true
 
+          // eslint-disable-next-line no-async-promise-executor
           return new Promise(async (resolve, reject) => {
             try {
               const { data } = await api.post('/sessions/refresh-token', {
@@ -69,6 +70,23 @@ api.registerInterceptTokenManager = (signOut) => {
                 token: data.token,
                 refresh_token: data.refresh_token,
               })
+
+              if (originalRequestConfig.data) {
+                originalRequestConfig.data = JSON.parse(
+                  originalRequestConfig.data,
+                )
+              }
+
+              originalRequestConfig.headers = {
+                Authorization: `Bearer ${data.token}`,
+              }
+              api.defaults.headers.common.Authorization = `Bearer ${data.token}`
+
+              failedQueue.forEach((request) => {
+                request.onSuccess(data.token)
+              })
+
+              resolve(api(originalRequestConfig))
             } catch (error: any) {
               failedQueue.forEach((request) => {
                 request.onFailure(error)
