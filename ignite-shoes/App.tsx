770a509662701789dd65d3d9a0b04fd59b5a964e
Rafael Souza <rpsouza.dev@gmail.com>
2023-09-03 20:30:23 -0300

    receiving notifications

--- ignite-shoes/App.tsx
+++ ignite-shoes/App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,9 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId(process.env.EXPO_PUBLIC_ONE_SIGNAL_ID);
+OneSignal.setEmail('rplinux@email.com')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
