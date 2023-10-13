634b0520d99748fe2f6a345a9a531a48283fa013
Rafael Souza <rpsouza.dev@gmail.com>
2023-10-13 10:42:24 -0300

    Add keyframe

--- ignitequiz/src/components/Question/index.tsx
+++ ignitequiz/src/components/Question/index.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 
 import { Option } from '../Option';
 import { styles } from './styles';
+import Animated, { Keyframe } from 'react-native-reanimated';
 
 type QuestionProps = {
   title: string;
@@ -14,9 +15,53 @@ type Props = {
   setAlternativeSelected?: (value: number) => void;
 }
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
 export function Question({ question, alternativeSelected, setAlternativeSelected }: Props) {
+
+  const enteringKeyframe = new Keyframe({
+    0: {
+      opacity: 0,
+      transform: [
+        { translateX: SCREEN_WIDTH },
+        { rotate: '90deg'}
+      ]
+    },
+    70: {
+      opacity: 0.3
+    },
+    100: {
+      opacity: 1,
+      transform: [
+        { translateX: 0 },
+        { rotate: '0deg'}
+      ]
+    },
+  })
+
+  const exitingKeyframe = new Keyframe({
+    from: {
+      opacity: 1,
+      transform: [
+        { translateX: 0 },
+        { rotate: '0deg'}
+      ]
+    },
+    to: {
+      opacity: 0,
+      transform: [
+        { translateX: SCREEN_WIDTH * (-1) },
+        { rotate: '-90deg'}
+      ]
+    }
+  })
+
   return (
-    <View style={styles.container}>
+    <Animated.View 
+    style={styles.container}
+    exiting={exitingKeyframe}
+    entering={enteringKeyframe}
+    >
       <Text style={styles.title}>
         {question.title}
       </Text>
@@ -31,6 +76,6 @@ export function Question({ question, alternativeSelected, setAlternativeSelected
           />
         ))
       }
-    </View>
+    </Animated.View>
   );
 }
