bbe1738b0d3a9faf4feb9aefa9ebabbd0fe77993
Rafael Souza <rpsouza.dev@gmail.com>
2023-10-15 09:21:17 -0300

    Add OverlayFeedback

---
+++ ignitequiz/src/components/OverlayFeedback/index.tsx
@@ -0,0 +1,32 @@
+import { BlurMask, Canvas, Rect } from "@shopify/react-native-skia";
+import { useWindowDimensions } from "react-native";
+import Animated from "react-native-reanimated";
+import { THEME } from "../../styles/theme";
+
+const STATUS = ['transparent', THEME.COLORS.BRAND_LIGHT, THEME.COLORS.DANGER_LIGHT];
+
+type OverlayFeedbackProps = {
+  status: number;
+}
+
+export default function OverlayFeedback({status}: OverlayFeedbackProps) {
+  const color = STATUS[status];
+
+  const { height, width } = useWindowDimensions()
+  
+  return (
+    <Animated.View style={{ height, width, position: 'absolute' }}>
+      <Canvas style={{flex: 1}}>
+      <Rect
+        x={0}
+        y={0}
+        width={width}
+        height={height}
+        color={color}
+      >
+        <BlurMask blur={50} style='inner' />
+      </Rect>
+      </Canvas> 
+    </Animated.View>
+  )  
+}
--- ignitequiz/src/screens/Quiz/index.tsx
+++ ignitequiz/src/screens/Quiz/index.tsx
@@ -17,6 +17,7 @@ import Animated, { Easing, Extrapolate, interpolate, runOnJS, useAnimatedScrollH
 import { ProgressBar } from '../../components/ProgressBar';
 import { THEME } from '../../styles/theme';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import OverlayFeedback from '../../components/OverlayFeedback';
 
 interface Params {
   id: string;
@@ -194,6 +195,8 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
+      <OverlayFeedback status={0} />
+
       <Animated.View style={fixedProgressBarStyles}>
         <Text style={styles.title}>
           {quiz.title}
