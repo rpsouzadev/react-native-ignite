342c24c5bf0d32738b5efe074cd44a169f15bf03
Rafael Souza <rpsouza.dev@gmail.com>
2023-10-15 10:38:54 -0300

    Animation events

--- ignitequiz/src/components/Question/index.tsx
+++ ignitequiz/src/components/Question/index.tsx
@@ -2,7 +2,7 @@ import { View, Text, Dimensions } from 'react-native';
 
 import { Option } from '../Option';
 import { styles } from './styles';
-import Animated, { Keyframe } from 'react-native-reanimated';
+import Animated, { Keyframe, runOnJS } from 'react-native-reanimated';
 
 type QuestionProps = {
   title: string;
@@ -13,11 +13,12 @@ type Props = {
   question: QuestionProps;
   alternativeSelected?: number | null;
   setAlternativeSelected?: (value: number) => void;
+  onUnmount: () => void;
 }
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
-export function Question({ question, alternativeSelected, setAlternativeSelected }: Props) {
+export function Question({ question, alternativeSelected, setAlternativeSelected, onUnmount }: Props) {
 
   const enteringKeyframe = new Keyframe({
     0: {
@@ -58,9 +59,14 @@ export function Question({ question, alternativeSelected, setAlternativeSelected
 
   return (
     <Animated.View 
-    style={styles.container}
-    exiting={exitingKeyframe}
-    entering={enteringKeyframe}
+      style={styles.container}
+      entering={enteringKeyframe.duration(400)}
+      exiting={exitingKeyframe.duration(400).withCallback((finished) => {
+        'worklet'
+        if (finished) {
+          runOnJS(onUnmount)();
+        }
+      })}
     >
       <Text style={styles.title}>
         {question.title}
--- ignitequiz/src/screens/Quiz/index.tsx
+++ ignitequiz/src/screens/Quiz/index.tsx
@@ -83,6 +83,7 @@ export function Quiz() {
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
       setStatusReply(1)
       setPoints(prevState => prevState + 1);
+      handleNextQuestion()
     }else {
       setStatusReply(2)
       shakeAnimation()
@@ -110,7 +111,12 @@ export function Quiz() {
   function shakeAnimation() {
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce}), 
-      withTiming(0, { duration: 400, easing: Easing.bounce})
+      withTiming(0, undefined, (finished) => {
+        'worklet';
+        if (finished){
+          runOnJS(handleNextQuestion)();
+        }
+      })
     )
   }
 
@@ -186,11 +192,11 @@ export function Quiz() {
     setIsLoading(false);
   }, []);
 
-  useEffect(() => {
-    if (quiz.questions) {
-      handleNextQuestion();
-    }
-  }, [points]);
+  // useEffect(() => {
+  //   if (quiz.questions) {
+  //     handleNextQuestion();
+  //   }
+  // }, [points]);
 
   if (isLoading) {
     return <Loading />
@@ -232,6 +238,7 @@ export function Quiz() {
               question={quiz.questions[currentQuestion]}
               alternativeSelected={alternativeSelected}
               setAlternativeSelected={setAlternativeSelected}
+              onUnmount={() => setStatusReply(0)}
             />
           </Animated.View>
         </GestureDetector>
