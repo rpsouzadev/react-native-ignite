dbfe3ad68f0d865a5d4c251c8313955928c87a1c
Rafael Souza <rpsouza.dev@gmail.com>
2023-10-22 15:24:30 -0300

    Ignite Fleet

---
+++ ignite-fleet/src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import {styles} from './styles'
+import {Text, View } from 'react-native';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      
+    </View>
+  );
+}
