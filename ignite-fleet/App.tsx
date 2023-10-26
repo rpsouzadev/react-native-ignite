6c2ee857a24387055ad2205a344e8999cc1a59e1
Rafael Souza <rpsouza.dev@gmail.com>
2023-10-26 16:57:39 -0300

    Add navigation

--- ignite-fleet/App.tsx
+++ ignite-fleet/App.tsx
@@ -9,7 +9,7 @@ import { AppProvider, UserProvider } from '@realm/react'
 
 import theme from './src/theme'
 
-import { Home } from '@screens/Home'
+import { Routes } from '@routes/index'
 import { SignIn } from './src/screens/SignIn'
 import { Loading } from '@components/Loading'
 
@@ -29,7 +29,7 @@ export default function App() {
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
         <UserProvider fallback={<SignIn />}>
-          <Home />
+          <Routes />
         </UserProvider>
         <StatusBar
           barStyle="light-content"
