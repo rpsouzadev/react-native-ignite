d28c543e6a77fc42b8c434bb357d0848c39d2665
Rafael Souza <rpsouza.dev@gmail.com>
2023-10-29 10:46:40 -0300

    Generate method

--- ignite-fleet/src/libs/realm/schemas/Historic.ts
+++ ignite-fleet/src/libs/realm/schemas/Historic.ts
@@ -1,7 +1,31 @@
 import { Realm } from '@realm/react'
 
+type GenerateProps = {
+  user_id: string
+  description: string
+  license_plate: string
+}
+
 export class Historic extends Realm.Object<Historic> {
-  static generate() {}
+  _id!: string
+  user_id!: string
+  license_plate!: string
+  description!: string
+  status!: string
+  created_at!: string
+  updated_at!: string
+
+  static generate({ user_id, description, license_plate }: GenerateProps) {
+    return {
+      _id: new Realm.BSON.UUID(),
+      user_id,
+      license_plate,
+      description,
+      status: 'departure',
+      created_at: new Date(),
+      update_at: new Date(),
+    }
+  }
 
   static schema = {
     name: 'Historic',
