74be4051a770d872278067768667cb98281e752d
Rafael Souza <rpsouza.dev@gmail.com>
2023-11-26 21:20:52 -0300

    Grouping tests with Describe

--- iweather/src/utils/getNextDays.test.ts
+++ iweather/src/utils/getNextDays.test.ts
@@ -1,7 +1,9 @@
 import { getNextDays } from "./getNextDays";
 
-it('should be return the next five days', () => {
-  const nextFiveDays = getNextDays()
-
-  expect(nextFiveDays.length).toBe(5)
+describe("getNextDays", () => {
+  it('should be return the next five days', () => {
+    const nextFiveDays = getNextDays()
+  
+    expect(nextFiveDays.length).toBe(5)
+  })
 })
