48b68c5d7dbb042e688721359db0c5931726aaae
Rafael Souza <rpsouza.dev@gmail.com>
2023-12-03 14:55:21 -0300

    TextMatch

---
+++ iweather/src/components/SelectList/__test__/selectList.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react-native";
+import { SelectList } from '@components/SelectList'
+
+describe('Component: SelectList', () => {
+  it('should be return city details selected', () => {
+    const data = [
+      {id: '1', name: 'Salvador', latitude: 153, longitude: 532},
+      {id: '2', name: 'Campo Grande', latitude: 477, longitude: 977},
+    ]
+
+    render(
+      <SelectList
+      data={data}
+      onChange={() => {}}
+      onPress={() => {}}
+    />)
+    
+    const selectedCity = screen.getByText(/Grande/i)
+    console.log(selectedCity)
+    
+  })
+})
