ed05bbe5e34d051aea96e933d02d9d3f6e41ee83
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-02 19:34:01 -0300

    Day component test

---
+++ iweather/src/components/Day/__test__/day.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react-native'
+import clearDay from '@assets/clear_day.svg'
+import { Day } from '../index'
+
+describe('Component: Day', () => {
+  it('should be render day', () => {
+    const { debug } = render(
+      <Day 
+        data={{ 
+          day: '18/07',
+          min: '30°c',
+          max: '34°c',
+          icon: clearDay,
+          weather: 'Céu limpo'
+        }}
+      />
+    )
+
+    debug();
+  })
+})
