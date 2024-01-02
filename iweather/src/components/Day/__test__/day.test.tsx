6255e77dccabf26817f1f2737d8f5e95bea85c56
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-02 20:11:55 -0300

    Finishing the Day component test

--- iweather/src/components/Day/__test__/day.test.tsx
+++ iweather/src/components/Day/__test__/day.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react-native'
+import { render, screen } from '@testing-library/react-native'
 import clearDay from '@assets/clear_day.svg'
 import { Day } from '../index'
 
 describe('Component: Day', () => {
   it('should be render day', () => {
-    const { debug } = render(
+    render(
       <Day 
         data={{ 
           day: '18/07',
@@ -16,6 +16,6 @@ describe('Component: Day', () => {
       />
     )
 
-    debug();
+    expect(screen.getByText('18/07')).toBeTruthy()
   })
 })
