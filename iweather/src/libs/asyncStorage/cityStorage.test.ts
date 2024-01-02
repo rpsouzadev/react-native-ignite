202391cdbab9ade15dd5e5513a25950b8f715a3c
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-02 18:54:03 -0300

    saveStorageCity test

--- iweather/src/libs/asyncStorage/cityStorage.test.ts
+++ iweather/src/libs/asyncStorage/cityStorage.test.ts
@@ -1,9 +1,24 @@
-import { getStorageCity } from "@libs/asyncStorage/cityStorage"
+import { getStorageCity, saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { CityProps } from "@services/getCityByNameService"
 
 describe('Storage: cityStorage', () => {
   it("should be return null when don't have a city storage", async () => {
     const response = await getStorageCity()
 
     expect(response).toBeNull()
+  })
+
+  it("should return city storage", async () => {
+    const newCity: CityProps = {
+      id: '1',
+      name: 'Salvador',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(newCity)
+    const city = await getStorageCity()
+
+    expect(city).toEqual(newCity)
   })
 })
