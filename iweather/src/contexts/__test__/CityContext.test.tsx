57e0fe22bed74d8ee6e92461f82b9d127fcbfc55
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-09 09:00:17 -0300

    Wrapper

--- iweather/src/contexts/__test__/CityContext.test.tsx
+++ iweather/src/contexts/__test__/CityContext.test.tsx
@@ -1,9 +1,10 @@
+import { CityProvider } from "@contexts/CityContext"
 import { useCity } from "@hooks/useCity"
 import { renderHook } from "@testing-library/react-native"
 
 describe("Context: CityContext", () => {
   it("should be change selected city", () => {
-    const {result} = renderHook(() => useCity())
-    console.log('result => ', result.current.city)
+    const {result} = renderHook(() => useCity(), { wrapper: CityProvider })
+    console.log('result => ', result.current)
   })
 })
