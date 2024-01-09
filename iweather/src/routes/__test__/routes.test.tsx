8e121839f634ab6c5b0e3fa4c44962d9a5539dc7
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-09 15:33:01 -0300

    CustonRender

--- iweather/src/routes/__test__/routes.test.tsx
+++ iweather/src/routes/__test__/routes.test.tsx
@@ -1,10 +1,28 @@
 import { Routes } from "../index"
-import { render } from "@testing-library/react-native"
+import { CityProps } from "@services/getCityByNameService"
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
+import { render, screen, waitFor } from "@__tests__/utils/customRender"
+
+const newCity: CityProps = {
+  id: '1',
+  name: 'Salvador',
+  latitude: 123,
+  longitude: 456
+}
 
 describe("Routes", () => {
-  it("should be render Search screen when not city selected.", () => {
-    const { debug } = render(<Routes />)
+  it("should be render Search screen when not city selected.", async () => {
+    render(<Routes />)
 
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+    expect(title).toBeTruthy()
+  })
+
+  it("should be render Dashboard screen when has city selected.", async () => {
+    await saveStorageCity(newCity)
+    const { debug } = render(<Routes />)
     debug()
+    // const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+    // expect(title).toBeTruthy()
   })
 })
