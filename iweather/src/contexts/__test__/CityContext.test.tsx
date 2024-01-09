8bcd33c500b274e0e51b9ce7152fa88bc46ead54
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-09 11:02:44 -0300

    Updating hook state in jest

--- iweather/src/contexts/__test__/CityContext.test.tsx
+++ iweather/src/contexts/__test__/CityContext.test.tsx
@@ -1,11 +1,21 @@
 import { useCity } from "@hooks/useCity"
 import { CityProvider } from "@contexts/CityContext"
+import { CityProps } from "@services/getCityByNameService"
 import { renderHook, waitFor, act } from "@testing-library/react-native"
+
+const newCity: CityProps = {
+  id: '1',
+  name: 'Salvador',
+  latitude: 123,
+  longitude: 456
+}
 
 describe("Context: CityContext", () => {
   it("should be change selected city", async () => {
     const {result} = renderHook(() => useCity(), { wrapper: CityProvider })
 
-    await waitFor(() => act(() => console.log('result => ', result.current)))
+    await waitFor(() => act(() => result.current.handleChanceCity(newCity)))
+
+    expect(result.current.city?.name).toBe(newCity.name)
   })
 })
