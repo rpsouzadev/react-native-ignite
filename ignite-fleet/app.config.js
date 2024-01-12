180be0139a5f7c5308640c2d184a128b76cffd06
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-11 22:11:09 -0300

    Using the Google Maps API Key

---
+++ ignite-fleet/app.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  expo: {
+    name: 'ignite fleet',
+    slug: 'ignite-fleet',
+    scheme: 'ignite-fleet',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'dark',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'cover',
+      backgroundColor: '#202024',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#202024',
+      },
+      package: 'com.rpsouza.ignitefleet',
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    plugins: [
+      [
+        'expo-location',
+        {
+          locationAlwaysAndWhenInUsePermission:
+            'Allow $(PRODUCT_NAME) to use your location.',
+        },
+      ],
+    ],
+  },
+}
