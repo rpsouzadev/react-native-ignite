8c4bbe5266ad69fc4a3192442aae5f3267134717
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-15 16:01:22 -0300

    Coords schema

--- ignite-fleet/src/libs/realm/schemas/Historic.ts
+++ ignite-fleet/src/libs/realm/schemas/Historic.ts
@@ -2,11 +2,13 @@
 import { Realm } from '@realm/react'
 
 import { ObjectSchema } from 'realm'
+import { CoordsSchemaProps } from './Coords'
 
 type GenerateProps = {
   user_id: string
   description: string
   license_plate: string
+  coords: CoordsSchemaProps[]
 }
 
 export class Historic extends Realm.Object<Historic> {
@@ -14,16 +16,23 @@ export class Historic extends Realm.Object<Historic> {
   user_id!: string
   license_plate!: string
   description!: string
+  coords!: CoordsSchemaProps[]
   status!: string
   created_at!: Date
   updated_at!: Date
 
-  static generate({ description, license_plate, user_id }: GenerateProps) {
+  static generate({
+    description,
+    license_plate,
+    user_id,
+    coords,
+  }: GenerateProps) {
     return {
       _id: new Realm.BSON.UUID(),
       user_id,
       license_plate,
       description,
+      coords,
       status: 'departure',
       created_at: new Date(),
       updated_at: new Date(),
@@ -39,6 +48,10 @@ export class Historic extends Realm.Object<Historic> {
       user_id: { type: 'string', indexed: true },
       license_plate: 'string',
       description: 'string',
+      coords: {
+        type: 'list',
+        objectType: 'Coords',
+      },
       status: 'string',
       created_at: 'date',
       updated_at: 'date',
