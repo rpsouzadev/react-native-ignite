2b7bf294d55bb7c6b22040dae174f2cdccfc195e
Rafael Souza <rpsouza.dev@gmail.com>
2024-01-15 16:07:23 -0300

    Registering the output in the database

--- ignite-fleet/src/screens/Departure/useDeparture.ts
+++ ignite-fleet/src/screens/Departure/useDeparture.ts
@@ -86,6 +86,13 @@ export function useDeparture() {
             user_id: user.id,
             license_plate: licensePlate.toUpperCase(),
             description,
+            coords: [
+              {
+                latitude: currentCoords.latitude,
+                longitude: currentCoords.longitude,
+                timestamp: new Date().getTime(),
+              },
+            ],
           }),
         )
       })
